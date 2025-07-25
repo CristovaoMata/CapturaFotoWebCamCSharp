75ca8e9e034f31f7277558a961f70bcdca239570
Cristóvão Mata 2 <cristovaomata2023@gmail.com>
2025-07-26 00:56:58 +0100

    Adicionar arquivos de projeto.

---
+++ CapturaFotoWebCam/FrmCapturaFoto.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CapturaFotoWebCam
+{
+    partial class FrmCapturaFoto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxCamera = new System.Windows.Forms.PictureBox();
+            this.pictureBoxFotoCapturada = new System.Windows.Forms.PictureBox();
+            this.btnCapturar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFotoCapturada)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxCamera
+            // 
+            this.pictureBoxCamera.Location = new System.Drawing.Point(27, 37);
+            this.pictureBoxCamera.Name = "pictureBoxCamera";
+            this.pictureBoxCamera.Size = new System.Drawing.Size(435, 383);
+            this.pictureBoxCamera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxCamera.TabIndex = 0;
+            this.pictureBoxCamera.TabStop = false;
+            // 
+            // pictureBoxFotoCapturada
+            // 
+            this.pictureBoxFotoCapturada.Location = new System.Drawing.Point(491, 63);
+            this.pictureBoxFotoCapturada.Name = "pictureBoxFotoCapturada";
+            this.pictureBoxFotoCapturada.Size = new System.Drawing.Size(275, 220);
+            this.pictureBoxFotoCapturada.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxFotoCapturada.TabIndex = 0;
+            this.pictureBoxFotoCapturada.TabStop = false;
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Location = new System.Drawing.Point(518, 318);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(93, 49);
+            this.btnCapturar.TabIndex = 1;
+            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(638, 318);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(93, 49);
+            this.btnSalvar.TabIndex = 1;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // FrmCapturaFoto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnCapturar);
+            this.Controls.Add(this.pictureBoxFotoCapturada);
+            this.Controls.Add(this.pictureBoxCamera);
+            this.Name = "FrmCapturaFoto";
+            this.Text = "FrmCapturaFoto";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmCapturaFoto_FormClosing);
+            this.Load += new System.EventHandler(this.FrmCapturaFoto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFotoCapturada)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxCamera;
+        private System.Windows.Forms.PictureBox pictureBoxFotoCapturada;
+        private System.Windows.Forms.Button btnCapturar;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
---
+++ CapturaFotoWebCam/FrmCapturaFoto.cs
@@ -0,0 +1,74 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CapturaFotoWebCam
+{
+    public partial class FrmCapturaFoto : Form
+    {
+
+        private FilterInfoCollection dispositivosVideo;
+        private VideoCaptureDevice videoSource;
+
+        public FrmCapturaFoto()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCapturaFoto_Load(object sender, EventArgs e)
+        {
+            dispositivosVideo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (dispositivosVideo.Count > 0)
+            {
+                videoSource = new VideoCaptureDevice(dispositivosVideo[1].MonikerString); 
+                videoSource.NewFrame += VideoSource_NewFrame;
+                videoSource.Start();
+            }
+            else
+            {
+                MessageBox.Show("Nenhuma câmera encontrada.");
+            }
+
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap imagem = (Bitmap)eventArgs.Frame.Clone();
+            pictureBoxCamera.Image = imagem;
+        }
+
+        private void btnCapturar_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxCamera.Image != null)
+            {
+                pictureBoxFotoCapturada.Image = (Bitmap)pictureBoxCamera.Image.Clone();
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxFotoCapturada.Image != null)
+            {
+                string caminho = Path.Combine(Application.StartupPath, "fotos");
+                Directory.CreateDirectory(caminho);
+
+                string nomeArquivo = Path.Combine(caminho, $"foto_{DateTime.Now.Ticks}.jpg");
+                pictureBoxFotoCapturada.Image.Save(nomeArquivo, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                MessageBox.Show("Foto Salva com sucessso:\n" + nomeArquivo);
+            }
+        }
+
+        private void FrmCapturaFoto_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+        }
+    }
+}
